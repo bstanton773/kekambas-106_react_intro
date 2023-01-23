ec0f94700855b1955402b3901e06c3f3611208df
Brian <bstanton773@gmail.com>
2023-01-23 14:30:21 -0600

    Add navbar with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
 
-
-function App(){
-  return <h1>Hello World</h1>
+function App(props){
+    let myName = 'Brian'
+    return (
+        <>
+            <Navbar city="Chicago" test={123} name={myName} />
+            <div className="container">
+                <h1>Hello {myName} {10 * 5}</h1>
+            </div>
+        </>
+        );
 }
 
 export default App;
