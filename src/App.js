f0430373f05315ba644500be8aebe452cdaa3532
Brian <bstanton773@gmail.com>
2023-01-23 15:13:13 -0600

    Add stateful function components with useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
 function App(props){
     let myName = 'Brian'
+    let buttons = [
+        {color: 'primary', step: 1},
+        {color: 'secondary', step: 10},
+        {color: 'success', step: 100},
+        {color: 'danger', step: 1000},
+    ]
     return (
         <>
             <Navbar city="Chicago" test={123} name={myName} />
             <div className="container">
                 <h1>Hello {myName} {10 * 5}</h1>
+                {buttons.map((button, idx) => <Button color={button.color} step={button.step} key={idx}/>)}
             </div>
         </>
         );
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export default function Button(props) {
+    const [count, setCount] = useState(0);
+
+    function handleClick(){
+        console.log('Button clicked', count, props.step)
+        setCount(count + props.step)
+    }
+
+    return (
+        <button className={`btn btn-${props.color} w-100`} onClick={handleClick}>+{props.step} - {count}</button>
+    )
+}
