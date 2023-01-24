7edc0f98cc563bb268928fe532ab0bdb3cdb5b97
Brian <bstanton773@gmail.com>
2023-01-24 13:54:22 -0600

    Add name and city state to App with navbar form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
 function App(props){
-    let myName = 'Brian'
     let buttons = [
         {color: 'primary', step: 1},
         {color: 'secondary', step: 10},
@@ -11,15 +10,26 @@ function App(props){
         {color: 'danger', step: 1000},
     ]
 
+    // Setting state variables (these variables will preserve their value across function calls i.e. new renders of component)
     const [count, setCount] = useState(0);
+    const [myName, setMyName] = useState('');
+    const [myCity, setMyCity] = useState('');
 
+    // Function to handle the button click, that will change the count variable to whatever it currently is plus the step
     function handleClick(step){
-        setCount(count + step)
+        let newCount = count + step
+        setCount(newCount)
     };
+
+    // Function that will update myName and myCity variables with whatever strings are passed into it
+    function updateUserInfo(username, usercity){
+        setMyName(username);
+        setMyCity(usercity);
+    }
 
     return (
         <>
-            <Navbar city="Chicago" test={123} name={myName} />
+            <Navbar city={myCity} name={myName} updateUser={updateUserInfo} />
             <div className="container">
                 <h1>Hello {myName}, Count: {count}</h1>
                 {buttons.map((button, idx) => <Button color={button.color} step={button.step} key={idx} handleClick={handleClick} count={count}/>)}
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function Button(props) {
-    console.log(props);
     return (
         <button className={`btn btn-${props.color} w-100`} onClick={() => props.handleClick(props.step)}>+{props.step} - {props.count}</button>
     )
