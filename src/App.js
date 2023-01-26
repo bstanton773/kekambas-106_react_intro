80d2266871a322df00f5fd6cf6485b56cfa8cae1
Brian <bstanton773@gmail.com>
2023-01-26 15:58:17 -0600

    Add React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
 import ButtonDisplay from "./components/ButtonDisplay";
 import Navbar from "./components/Navbar";
 import RacerDisplay from "./components/RacerDisplay";
@@ -20,12 +21,15 @@ function App(props){
         <>
             <Navbar city={myCity} name={myName} updateUser={updateUserInfo} />
             <div className="container">
-                <ButtonDisplay myName={myName} />
-                {/* <RacerDisplay /> */}
-                <RacerDisplayClass />
+                <Routes>
+                    <Route path='/' element={<ButtonDisplay myName={myName} />} />
+                    {/* <Route path='/standings' element={<RacerDisplay />} /> */}
+                    <Route path='/standings' element={<RacerDisplayClass />} />
+                </Routes>
             </div>
         </>
     );
 }
 
 export default App;
+    
