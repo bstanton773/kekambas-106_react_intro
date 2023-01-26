957b2a77470d666eb47e673aa788a543bc734833
Brian <bstanton773@gmail.com>
2023-01-26 16:55:17 -0600

    Update handleRegister to create new user

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,7 +4,39 @@ export default class Register extends Component {
 
     handleRegister = event => {
         event.preventDefault();
-        console.log(event);
+        // console.log(event);
+        let password = event.target.password.value;
+        let confirmPass = event.target.confirmPass.value;
+        if (password !== confirmPass){
+            console.warn('Passwords do not match');
+        } else {
+            console.log('Passwords match');
+
+            // Set up the request
+            let myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json');
+
+            let formData = JSON.stringify({
+                username: event.target.username.value,
+                email: event.target.email.value,
+                password
+            })
+
+            fetch("http://localhost:5000/api/users", {
+                method: 'POST',
+                headers: myHeaders,
+                body: formData
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.error){
+                        console.error(data.error)
+                    } else {
+                        console.log(data)
+                    }
+                })
+        }
+
     }
 
 
